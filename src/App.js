0349236e07f1cd9067a5d6803a9a1fcc3a5401a4
dasaman15844 <dasaman15844@gmail.com>
2022-04-06 21:37:22 +0530

    install

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <form>
+    <div className='col-75'>
+      Enter your name: <div>
+        <input type="text" name='name'/>
+        </div>
+        </div><div className='col-75'>
+        
+      Enter your password:<div>
+        <input type="text" name='password'/>
+        </div>
+        </div>
+
+        <div className='col-75'>
+        
+      Address:<div>
+        <input type="text" name='password'/>
+        </div>
+        </div>
+
+        <div className='col-75'>
+        Gender:<br />
+        <input type="radio" id="Male" name="fav_language" value="Male"/> 
+        <label for="Male">Male</label><br/>
+        <input type="radio" id="Female" name="fav_language" value="Female" />
+        <label for="female">Female</label>
+       
+
+          </div>
+
+<div className='col-75'>Country : <br/>
+          <select name="Country" id="country-select">
+    <option value="">--Please choose an option--</option>
+    <option value="Nepal">Nepal</option>
+    <option value="India">India</option>
+    <option value="Japan">Japan</option>
+    <option value="Spain">Spain</option>
+    <option value="Australia">Australia</option>
+    <option value="Usa">Usa</option>
+</select>
+
+</div>
+
+<div className='col-75' > Choose your Language : <br/>
+<input type="checkbox" id="English" name="English" value="English"/>
+  <label for="English"> English</label><br />
+  <input type="checkbox" id="Hindi" name="Hindi" value="Hindi" />
+  <label for="Hindi"> Hindi</label><br />
+  <input type="checkbox" id="Neplai" name="Nepali" value="Nepali"/>
+  <label for="Neplai"> Nepali</label><br /><br />
+  </div>
+        
+        <div className='row'>
+    <input type="submit" value="Submit"  />
+  </div>
+    </form>
     </div>
   );
 }
